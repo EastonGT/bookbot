2b9f85ed6af8645796e3f2e0d5863669cd6b3e3b
EastonGT <gregeaston@gmail.com>
2025-04-26 19:21:35 +0100

    First change, file can now print the .txt files

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    print(book_text)
+
+if __name__ == "__main__":
+    main()
